82a8b0a3f88587b9035a6a27f9d9e7ad44076c91
luishsr <luishsoares@gmail.com>
2023-04-26 22:29:15 -0300

    Updated comments

--- filesys.go
+++ filesys.go
@@ -1,3 +1,6 @@
+// Filesys package is a simple distributed file system (dfs) implementation
+// demonstrating one of the Web3 features
+// TO-DO: implement remaining CRUD operations
 package filesys
 
 import (
@@ -8,23 +11,27 @@ import (
 	"os"
 )
 
+// Stores Node info
 type Node struct {
 	Address string
 	Port    int
 }
 
+// Stores File metadata
 type FileMetadata struct {
 	FileName string
 	Size     int64
 	Node     Node
 }
 
+// Stores an array of connected nodes and its file metadata
 type SimpleDistributedFileSystem struct {
 	metadata map[string]FileMetadata
 	nodes    []Node
 	client   *http.Client
 }
 
+// Starts a new File system component (exposed constructor)
 func NewSimpleDistributedFileSystem(nodes []Node) *SimpleDistributedFileSystem {
 	return &SimpleDistributedFileSystem{
 		metadata: make(map[string]FileMetadata),
@@ -33,14 +40,19 @@ func NewSimpleDistributedFileSystem(nodes []Node) *SimpleDistributedFileSystem {
 	}
 }
 
+// Broadcast upload request to connected Nodes
 func (s *SimpleDistributedFileSystem) Put(file *os.File) error {
+
+	// Collects file info from os.File
 	fileInfo, err := file.Stat()
 	if err != nil {
 		return err
 	}
 
+	// Loads nodes file's metadata
 	node := s.nodes[len(s.metadata)%len(s.nodes)]
 
+	// Formats PUT http request (upload)
 	url := fmt.Sprintf("http://%s:%d/upload/%s", node.Address, node.Port, fileInfo.Name())
 	req, err := http.NewRequest(http.MethodPut, url, file)
 	if err != nil {
@@ -64,12 +76,16 @@ func (s *SimpleDistributedFileSystem) Put(file *os.File) error {
 	return nil
 }
 
+// Broadcast download request to connected Nodes
 func (s *SimpleDistributedFileSystem) Get(fileName string) (*os.File, error) {
+
+	// Collects file info from os.Files
 	metadata, ok := s.metadata[fileName]
 	if !ok {
 		return nil, errors.New("file not found")
 	}
 
+	// Formats GET http request (download)
 	url := fmt.Sprintf("http://%s:%d/download/%s", metadata.Node.Address, metadata.Node.Port, fileName)
 	resp, err := s.client.Get(url)
 	if err != nil {
@@ -81,6 +97,7 @@ func (s *SimpleDistributedFileSystem) Get(fileName string) (*os.File, error) {
 		return nil, errors.New("failed to download file")
 	}
 
+	// Creates the file locally (download)
 	tempFile, err := os.CreateTemp("", fileName)
 	if err != nil {
 		return nil, err
